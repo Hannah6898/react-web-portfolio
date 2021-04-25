eb04c9cae94774d825cc8283029e0a2feffd69b8
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-25 11:50:57 +0100

    Refactored code

--- src/About.js
+++ src/About.js
@@ -11,10 +11,10 @@ export default function About() {
           <p className="text-sm-start">
             I am a front-end developer who through self-motivation and hard work
             has learnt to code whilst working full time. I am competent in
-            several coding languages including React and JavaScript and eager to
-            learn more. I have proven my skills through developing projects such
-            as React weather and dictionary apps which you can find linked
-            below. Currently I’m seeking a long-term position where I can
+            several coding languages including React and JavaScript, and am
+            continuing to further my learning. I have proven my skills through
+            developing projects such as a React weather app which you can find
+            linked below. Currently I’m seeking a long-term position where I can
             utilise my transferable skills such as problem solving and data
             analysis; and increase my knowledge.
           </p>
